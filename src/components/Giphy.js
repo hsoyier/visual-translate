b43916669147955d97a50b79ee6ccd1aa0107971
rayyo <reiyoshizawa0418@gmail.com>
2018-10-31 07:34:45 -0700

    Update getApi function

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -16,15 +16,20 @@ class Giphy extends Component {
     // const artist_count = 100;
     // const search = "japan";
     // const data = await fetch(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${api_key}&limit=${artist_count}`);
-    const data = await fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${api_key}`);
+    const data = await fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=50`);
     if(data.status === 200) {
       const json = await data.json();
       console.log(json.data);
       const giphys = [];
+      json.data.sort((a, b) => {
+        return (a.title > b.title ? 1 : -1);
+      });
       json.data.forEach(giphy => {
-        console.log(giphy.id);
+        console.log(json.data);
         giphys.push(
             <li key={giphy.id} className="giphy__item">
+              <p>@{giphy.username}</p>
+              <p>{giphy.title}</p>
               <img src={giphy.images.original.url} alt="" className="" />
             </li>
         );
