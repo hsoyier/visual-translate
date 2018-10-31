48a6beaa002b304a4db0f1127ccad7e4f777d21e
rayyo <reiyoshizawa0418@gmail.com>
2018-10-30 19:37:21 -0700

    first commit

---
+++ src/components/ArtistList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ArtistList = () => {
+  return (
+    <div>
+      <p>name</p>
+      <p>@</p>
+      <p>icon</p>
+      <p>Giphy</p>
+    </div>
+  )
+}
+
+export default ArtistList;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <div className="header__inner">
+        <h1 className="title">GIPHY RANKING</h1>  
+        {/* <div className="search">
+          <input type="text" name="" className="search__text" id=""/>
+          <button type="submit" className="search__btn" ><i class="far fa-search"></i></button>
+        </div> */}
+      </div>
+    </header>
+  )
+}
+
+export default Header;
+
+
