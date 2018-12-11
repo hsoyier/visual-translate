f7e3465d7b27be723189bdad2f0008078121db92
rayyo <reiyoshizawa0418@gmail.com>
2018-12-10 19:53:18 -0800

    Add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__inner">
+        <div className="logo" />
         <h1 className="title">Visual Translate</h1>
       </div>
     </header>
